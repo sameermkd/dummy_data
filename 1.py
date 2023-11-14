4629595192833918f7866f75dca64fb305f44c39
Sameer Kongath <zoomixinfo@gmail.com>
2023-11-14 22:15:21 +0530

    Dummy Data

---
+++ 1.py
@@ -0,0 +1,13 @@
+from faker import Faker
+
+fake=Faker()
+
+print(fake.name())
+print(fake.address())
+print(fake.text())
+print(fake.email())
+print(fake.country())
+print(fake.latitude(), fake.longitude())
+print(fake.url())
+
+#pip install faker
